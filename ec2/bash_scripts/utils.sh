5cfef257c86134b3d0713551768a510c6840323d
phunc20 <wucf20@gmail.com>
2022-08-05 14:27:30 +0700

    add convenient wrapper to call aws-cli commands (getip, start, stop, login)

--- ec2/bash_scripts/utils.sh
+++ ec2/bash_scripts/utils.sh
@@ -1,5 +1,5 @@
 t2s() {
-    sed 's/d/*24*3600 +/g; s/h/*3600 +/g; s/m/*60 +/g; s/s/\+/g; s/+[ ]*$//g' <<< "$@" | bc
+    #sed 's/d/*24*3600 +/g; s/h/*3600 +/g; s/m/*60 +/g; s/s/\+/g; s/+[ ]*$//g' <<< "$@" | bc
 }
 
 
@@ -23,4 +23,35 @@ stopme() {
         n_seconds=$(t2s $@)
         yes | pv -SL1 -F "Stop instance $instance_id in %e" -s $n_seconds > /dev/null && aws ec2 stop-instances --instance-ids $instance_id
     fi
+}
+
+
+ec2getip() {
+    aws ec2 describe-instances \
+        --query "Reservations[*].Instances[*].PublicIpAddress" \
+        --output=text \
+        --instance-ids $@
+}
+
+
+ec2start() {
+    aws ec2 start-instances --instance-ids $@
+}
+
+
+ec2stop() {
+    aws ec2 stop-instances --instance-ids $@
+}
+
+
+ec2login() {
+    if [ "$#" -ne 1 ]; then
+        echo "Exactly one input (i.e. instance-id) is required."
+        echo "Your input arg is wrong. Please try again"
+        exit 1
+    fi
+    ec2start $1
+    sleep 5s
+    ip=$(ec2getip $1)
+    ssh -i $HOME/.ssh/top_secret.cer "ubuntu@$ip"
 }
